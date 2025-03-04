d99c476b7bb1a72cf55f6a131efa80b019d13cca
SharonGur27 <85168742+SharonGur27@users.noreply.github.com>
2025-03-04 03:38:08 +0200

    Delete Module 6 directory

--- Module 6/StarkIndustries/Deployment/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using Deployment;
-
-var distribution = new Distribution();
-await distribution.StartDistributionAsync();
-
-Console.WriteLine("Distribution completed. Press any key to exit...");
-Console.ReadKey();
--- Module 6/StarkIndustries/Manufacturing/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using Manufacturing;
-
-var productionLine = new ProductionLine();
-await productionLine.StartProductionAsync();
-
-Console.WriteLine("Production completed. Press any key to exit...");
-Console.ReadKey();
